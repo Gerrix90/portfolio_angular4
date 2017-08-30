92c687c64fb7666fdc5aded5f78c3728bf0b3294
Magdalena Pal <magdalena.pal90@gmail.com>
2017-08-30 14:36:22 +0200

    Complete section about skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { AboutMeComponent } from './about-me/about-me.component';
 import { InitialNavComponent } from './initial-nav/initial-nav.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgsRevealModule } from 'ng-scrollreveal'; 
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     AppComponent,
     InitialNavComponent,
     AboutMeComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    PortfolioComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
