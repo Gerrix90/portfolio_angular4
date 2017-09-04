6f7af24ad0237473b312294aaf73bc5896d3b4cc
Magdalena Pal <magdalena.pal90@gmail.com>
2017-09-04 18:39:43 +0200

    Correct RWD. Complete section contact-form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { Validators, FormGroup,  FormControl} from '@angular/forms';
+import { DOCUMENT } from '@angular/platform-browser';
+import { NgZone } from '@angular/core';
 
 
 @Component({
@@ -9,6 +11,8 @@ import { Validators, FormGroup,  FormControl} from '@angular/forms';
 })
 export class ContactFormComponent implements OnInit {
 
+  public isActive: boolean = false;
+  
 	private formSubmitAttempt: boolean;
 
   form = new FormGroup({
@@ -29,10 +33,20 @@ export class ContactFormComponent implements OnInit {
     form.valid;
   }
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit() {
 
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    let number = this.document.body.scrollTop;
+    if (number > 5000) {
+      this.isActive = true;
+    } else if (this.isActive && number < 10) {
+      this.isActive = false;
+    }
   }
 
 	onSubmit() {
