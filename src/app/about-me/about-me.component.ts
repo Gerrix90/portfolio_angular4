0bac5aaa7e529cd121cc6f0a192fdb02052a1b9a
Magdalena Pal <magdalena.pal90@gmail.com>
2017-09-05 21:49:46 +0200

    Improve displaying section about me

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,29 +9,32 @@ import { NgZone } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
-  public isActive: boolean = false;
+  public isDesktop: boolean;
+  public isActive: boolean;
   
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
   
 
   ngOnInit() {
-
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+      this.isDesktop = true;
+      this.isActive = false;
+    } else {
+      this.isDesktop = false;
+      this.isActive = true;
+    }
   }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
-    let number = this.document.body.scrollTop;
-    if (number > 200) {
-      this.isActive = true;
-    } else if (this.isActive && number < 10) {
-      this.isActive = false;
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+      let number = this.document.body.scrollTop;
+      if (number > 200) {
+        this.isActive = true;
+      } else if (this.isActive && number < 10) {
+        this.isActive = false;
+      }
     }
   }
 }
-
-
- 
-
- 
- 
