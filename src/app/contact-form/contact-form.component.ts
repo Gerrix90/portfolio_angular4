f1014022b15e9793fac2c01fc769a23888e60efb
Magdalena Pal <magdalena.pal90@gmail.com>
2017-09-07 13:53:24 +0200

    Try fixed bug on sent form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, HostListener, Inject } from '@angular/core';
 import { Validators, FormGroup,  FormControl} from '@angular/forms';
 import { DOCUMENT } from '@angular/platform-browser';
 import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 
 
 
@@ -20,7 +20,7 @@ export class ContactFormComponent implements OnInit {
   form = new FormGroup({
   	name: new FormControl('', [
   		Validators.required,
-  		Validators.minLength(5)
+  		Validators.minLength(2)
     ]),
     email: new FormControl('', Validators.required),
     message: new FormControl('', Validators.required)
@@ -45,17 +45,19 @@ export class ContactFormComponent implements OnInit {
 
 	onSubmit() {
     this.formSubmitAttempt = true;
-    let headers = new Headers({ 
-      'Accept': 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded'
-    });
-    let options = new RequestOptions({ headers: headers });
-    let url = "https://formspree.io/magdalena.pal90@gmail.com";
-    let data = `name=${this.name.value}&email=${this.email.value}&message=${this.message.value}`;
+    if (this.form.valid) {
+      let headers = new Headers({ 
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      });
+      let options = new RequestOptions({ headers: headers });
+      let url = "https://formspree.io/magdalena.pal90@gmail.com";
+      let data = `name=${this.name.value}&email=${this.email.value}&message=${this.message.value}`;
 
-    this.http.post(url, data, options).subscribe(
-      data => { alert("Message sent, thank you!") },
-      err => { alert("Message not sent, technical problems :(") }
-    );
-  }
+      this.http.post(url, data, options).subscribe(
+        data => { alert("Message sent, thank you!") },
+        err => { alert("Message not sent, technical problems :(") }
+      );
+    }
+   }
 };
