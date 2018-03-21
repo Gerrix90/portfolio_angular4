7159050cb91417ccecffd51744cca5d1b219cbf1
magdapal <magdalena.pal90@gmail.com>
2018-03-21 21:37:20 +0100

    Change display introduction on mobile

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -8,8 +8,17 @@ import { NgZone } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
-  
-  ngOnInit() {
 
+  public isDesktop: boolean;
+  public isActive: boolean;
+
+  ngOnInit() {
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+      this.isDesktop = true;
+      this.isActive = true;
+    } else {
+      this.isDesktop = false;
+      this.isActive = false;
+    }
   }
 }
